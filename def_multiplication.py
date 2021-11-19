20384168e4a83026d43825f7578a3357c50e6a96
kymhd <mahabamema15@gmail.com>
2021-11-19 00:56:40 +0000

    modify and add a Code

---
+++ def_multiplication.py
@@ -0,0 +1,37 @@
+def table_multiplication(base):
+    """
+    une fonction très simple qui permet de daire la multiplication 
+    d'un nombre.
+    """
+    for i in range(1, 12):
+        print(f" {i} x {base} = ", base*i)
+
+print(table_multiplication(5))
+"""
+1 x 5 =  5
+ 2 x 5 =  10
+ 3 x 5 =  15
+ 4 x 5 =  20
+ 5 x 5 =  25
+ 6 x 5 =  30
+ 7 x 5 =  35
+ 8 x 5 =  40
+ 9 x 5 =  45
+ 10 x 5 =  50
+ 11 x 5 =  55
+"""
+print(table_multiplication(3))
+"""
+1 x 3 =  3
+ 2 x 3 =  6
+ 3 x 3 =  9
+ 4 x 3 =  12
+ 5 x 3 =  15
+ 6 x 3 =  18
+ 7 x 3 =  21
+ 8 x 3 =  24
+ 9 x 3 =  27
+ 10 x 3 =  30
+ 11 x 3 =  33
+"""
+
